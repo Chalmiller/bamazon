66a46bcd327d8f6a769a4b7ba1e9f3b86b52d190
Christopher Miller <christopher@transcriptic.com>
2017-08-14 21:16:57 -0700

    got around to fixing some bugs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,67 +2,92 @@ var mysql = require("mysql");
 var inquirer = require("inquirer")
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "bamazon_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon_db"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  queryAllItems();
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    runSearch();
 });
 
 function queryAllItems() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
-    }
-    console.log("-----------------------------------");
-    runSearch();
-  });
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
+        console.log("-----------------------------------");
+    });
 }
 
 function queryIDs() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    var itemslist;
-    for (var i=0; i < res.length; i++) {
-      itemslist += res[i].item_id;
-    }
-    return itemslist;
-  })
+    connection.query("SELECT * FROM products", function(err, res) {
+        var itemslist;
+        for (var i = 0; i < res.length; i++) {
+            itemslist += res[i].item_id;
+        }
+        return itemslist;
+    })
+}
+
+function updateProducts(newQuery) {
+    connection.query(newQuery, function(err, result) {
+        console.log(result)
+    })
 }
 
 function runSearch() {
-  inquirer
-    .prompt([{
-      name: "idChoice",
-      type: "input",
-      message: "What would you like to purchase?",
-    },
-    {
-      name: "itemQuantity",
-      type: "input",
-      message: "How many would you like to purchase?",
-
-    }])
-    .then(function(answer) {
-      var query = "SELECT * FROM products";
-      connection.query(query, { item_id: answer.idChoice }, function(err, res) {
-        if (answer.itemQuantity > res[i].stock_quantity) {
-          console.log("Insufficient Quantity!");
-        } else {
-          var newQuantity = res[i].stock_quantity - answer.itemQuantity;;
-          var newQuery = "UPDATE products"
-          newQuery += "SET itemQuantity = " + newQuantity;
-          newQuery += "WHERE item_id = " + answer.idChoice;
-          connection.query(newQuery, function(err, res) {
-            console.log(result.affectedRows + " record(s) updated");
-          });
-        }
-      })
-    })
-  };
+    queryAllItems();
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        inquirer
+            .prompt([{
+                name: "product_name",
+                type: "rawlist",
+                choices: function() {
+                    var chosenItem = []
+                    for (var i = 0; i < res.length; i++) {
+                        chosenItem.push(res[i].product_name);
+                    }
+                    return chosenItem;
+                },
+                message: "What would you like to purchase?",
+            }, {
+                name: "itemQuantity",
+                type: "input",
+                message: "How many would you like to purchase?",
+            }])
+            .then(function(answer) {
+                var Item;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].product_name === answer.product_name) {
+                        Item = res[i];
+                    }
+                }
+                console.log(Item.id)
+                if (Item.stock_quantity < answer.itemQuantity) {
+                    console.log("Insufficient Quantity!");
+                    console.log("Try Again!");
+                    runSearch();
+                } else {
+                    var itemPrice = (Item.price * answer.itemQuantity);
+                    console.log("Your total comes to $" + itemPrice)
+                    var newQuantity = Item.stock_quantity - parseInt(answer.itemQuantity);
+                    connection.query("UPDATE products SET ? WHERE ?", [{
+                        stock_quantity: newQuantity,
+                    }, {
+                        product_name: Item.product_name
+                    }], function(err, result) {
+                        if (err) throw err;
+                        console.log(result.affectedRows + " record(s) updated");
+                    })
+                  runSearch();
+                }
+            });
+    });
+};
